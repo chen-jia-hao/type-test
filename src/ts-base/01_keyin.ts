969edbd58b0e53bf5d2ec16474039cd5cd6ff67d
cjh <chen-jh@outlook.com>
2022-08-26 16:35:23 +0800

    init porject

---
+++ src/ts-base/01_keyin.ts
@@ -0,0 +1,19 @@
+import {Equal, Expect} from "@type-challenges/utils"
+
+type FriendList = {
+  count: number;
+  friends: {
+    firstName: string;
+    lastName: string;
+  }[];
+};
+
+type Friends = FriendList["friends"];
+
+type Friend = Friends[number]
+
+type Fri = FriendList['friends'][123]
+
+type cases = [
+  Expect<Equal<Fri, Friend>>
+]
